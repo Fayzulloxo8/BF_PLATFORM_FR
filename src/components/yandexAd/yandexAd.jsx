519be6887e5b29b97480d42be729f8a2fcae9a55
fayzulloxo8 <solijonovfayzullox@gmail.com>
2025-07-01 15:55:05 +0500

    .

--- src/components/yandexAd/yandexAd.jsx
+++ src/components/yandexAd/yandexAd.jsx
@@ -3,20 +3,18 @@ import { useEffect } from 'react';
 
 const YandexAd = () => {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.innerHTML = `
-      window.yaContextCb = window.yaContextCb || [];
-      window.yaContextCb.push(() => {
-        Ya.Context.AdvManager.render({
-          blockId: "R-A-16091462-1",
-          renderTo: "yandex_rtb_R-A-16091462-1"
-        })
+    // Har ehtimolga qarshi yaContextCb arrayi mavjudligini tekshiramiz
+    window.yaContextCb = window.yaContextCb || [];
+
+    window.yaContextCb.push(() => {
+      window.Ya?.Context?.AdvManager?.render({
+        blockId: "R-A-16091462-1",
+        renderTo: "yandex_rtb_R-A-16091462-1"
       });
-    `;
-    document.body.appendChild(script);
+    });
   }, []);
 
-  return <div id="yandex_rtb_R-A-16091462-1" style={{marginTop: '15px'}}></div>;
+  return <div id="yandex_rtb_R-A-16091462-1" />;
 };
 
 export default YandexAd;
